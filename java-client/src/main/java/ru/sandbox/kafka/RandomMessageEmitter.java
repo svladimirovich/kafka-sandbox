68b2db6341d69f1b4132a68756d09a126d11ddc8
s.vladimirovich <mikser.vladimirovich@yandex.ru>
2018-11-29 20:07:32 +0300

    safe kafka producer settings

--- java-client/src/main/java/ru/sandbox/kafka/RandomMessageEmitter.java
+++ java-client/src/main/java/ru/sandbox/kafka/RandomMessageEmitter.java
@@ -34,6 +34,17 @@ public class RandomMessageEmitter {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // ******************************** safe Producer settings **************************************
+        // ENABLE_IDEMPOTENCE_CONFIG available since Kafka 0.11
+        // avoids duplicate messages on retries in case if ack response failed
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        // IDEMPOTENCE works with 1 MAX request per connection
+        // but since Kafka 1.1, more than 1 can be used safely and will maintain order of messages
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+        // **********************************************************************************************
+
         Producer kafkaProducer = new Producer(properties);
         this.emitter.blockingSubscribe(message -> {
             kafkaProducer.push("partitioned-topic", message.toJsonString());
