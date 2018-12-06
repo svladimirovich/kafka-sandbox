a36c28d74209a27951cdcdba21544c485cccf8d0
s.vladimirovich <mikser.vladimirovich@yandex.ru>
2018-12-06 18:57:16 +0300

    live twitter feed producer

---
+++ java-client/src/main/java/ru/sandbox/kafka/TwitterApiConfig_example.java
@@ -0,0 +1,10 @@
+package ru.sandbox.kafka;
+
+public class TwitterApiConfig_example {
+    // Twitter App credentials for API access
+    public final static String consumerKey = "";
+    public final static String consumerSecret = "";
+    public final static String token = "";
+    public final static String secret = "";
+
+}
---
+++ java-client/src/main/java/ru/sandbox/kafka/TwitterProducer.java
@@ -0,0 +1,98 @@
+package ru.sandbox.kafka;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterProducer {
+
+
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+
+    public TwitterProducer() {
+    }
+
+    public void run() {
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+
+        Client client = createTwitterClient(msgQueue, "putin");
+        client.connect();
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        Producer producer = new Producer(properties);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutdown signal caught!");
+            client.stop();
+            producer.close();
+        }));
+
+        while(!client.isDone()) {
+            String message = null;
+            try {
+                message = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+            if(message != null) {
+                System.out.println(message);
+                producer.push("tweeter_feed", message);
+            }
+        }
+        System.out.println("The reading of tweets is done!");
+    }
+
+    public Client createTwitterClient(BlockingQueue<String> msgQueue, String... keywords) {
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        // Optional: set up some followings and track terms
+        //List<Long> followings = Lists.newArrayList(1234L, 566788L); // this is to follow people
+        List<String> terms = Lists.newArrayList(keywords);
+        //hosebirdEndpoint.followings(followings);
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1(
+            TwitterApiConfig.consumerKey,
+            TwitterApiConfig.consumerSecret,
+            TwitterApiConfig.token,
+            TwitterApiConfig.secret
+        );
+
+        ClientBuilder builder = new ClientBuilder()
+            .name("Hosebird-Client-01")                              // optional: mainly for the logs
+            .hosts(hosebirdHosts)
+            .authentication(hosebirdAuth)
+            .endpoint(hosebirdEndpoint)
+            .processor(new StringDelimitedProcessor(msgQueue));
+
+        Client hosebirdClient = builder.build();
+        return hosebirdClient;
+    }
+}
