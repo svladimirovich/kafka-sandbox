6f0c0bda9a7e215f0a5ecf23114bc50851cf8f02
s.vladimirovich <mikser.vladimirovich@yandex.ru>
2019-01-14 19:43:35 +0300

    added docker container definition for word-count kafka stream app

--- java/word-count-stream/src/main/java/ru/sandbox/kafka/WordCountStreamApp.java
+++ java/word-count-stream/src/main/java/ru/sandbox/kafka/WordCountStreamApp.java
@@ -19,7 +19,7 @@ public class WordCountStreamApp {
 
         Properties properties = new Properties();
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count");
-        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
